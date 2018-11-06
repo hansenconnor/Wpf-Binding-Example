b254d79865455a305ce9fd46c294635a8742483f
hanse174 <hanse174@JB202-L21-W-D>
2018-11-06 14:42:34 -0500

    DataTemplates

---
+++ DataTemplates/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new Person { Age = 10, Name = "Bart" };
+
+            _list.ItemsSource = new ObservableCollection<Person>
+            {
+                new Person { Name = "Bart", Age = 10 },
+                new Person { Name = "Homer", Age = 45 },
+                new Person { Name = "Marge", Age = 35 }
+            };
+        }
+    }
+}
