dce9ae5a21ccba01af72c56fdedf835aa2c41495
hanse174 <hanse174@JB202-L21-W-D>
2018-11-08 14:18:04 -0500

    Working on ValueConverters

---
+++ WeatherForecast/Forecast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecast
+{
+    enum GeneralForecast
+    {
+        Sunny,
+        Rainy,
+        Snowy,
+        Cloudy,
+        Dry
+    }
+
+    class Forecast
+    {
+        public GeneralForecast GeneralForecast { get; set; }
+        public double TemperatureHigh { get; set; }
+        public double TemperatureLow { get; set; }
+        public double Percipitation { get; set; }
+        
+    }
+}
---
+++ WeatherForecast/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherForecast
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _days.ItemsSource = Enumerable.Range(1, 10);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var data = new List<Forecast>();
+            int days = (int)_days.SelectedItem;
+            var rnd = new Random();
+            for (int i = 0; i < days; i++)
+            {
+                double temp = rnd.NextDouble() * 40 - 10;
+                var forecast = new Forecast
+                {
+                    GeneralForecast = (GeneralForecast)rnd.Next(
+                        Enum.GetValues(typeof(GeneralForecast)).Length),
+                    TemperatureLow = temp,
+                    TemperatureHigh = temp + rnd.NextDouble() * 15,
+                    Percipitation = rnd.Next(10) > 5 ? rnd.NextDouble() * 10 : 0
+                };
+                data.Add(forecast);
+            }
+            DataContext = data;
+        }
+    }
+}
